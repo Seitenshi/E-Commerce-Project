8f72a3be45ca57861dd8b58174d433e0ff0ec6b4
Seitenshi <darkestdisguise@gmail.com>
2024-06-02 00:06:59 +0800

    Search bar

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -1,4 +1,24 @@
 <div>
     <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
-    you can start coding here
+    
+    <!-- Sticky Nav Bar -->
+    <div>
+        Nav
+    </div>
+
+    <br>
+    <!-- Big ass search bar -->
+    <div class="container">
+        <div class="row">
+            <form action="">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+              </form>
+        </div>
+    </div>
+
+
+
+
+
 </div>
