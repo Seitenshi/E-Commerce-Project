7e90e2ef88e67dab1dfaa74f29c4d1eb8a0c2153
Seitenshi <darkestdisguise@gmail.com>
2024-06-14 20:21:27 +0800

    about us / main page / routing additions

--- app/Http/Controllers/MainHome.php
+++ app/Http/Controllers/MainHome.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainHome extends Controller
 {
-    //
+    //Main Homepage Logged In
+    public function homepageLogged(){
+        return view("Homepage_login");
+    }
+
+    //about us
+    public function about(){
+        return view("Aboutpage");
+    }
 }
