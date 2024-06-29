842e1e3a950e06c2f4bbdb4564b3740d08baa6f6
Seitenshi <darkestdisguise@gmail.com>
2024-06-30 01:18:23 +0800

    a

---
+++ resources/views/Checkout/addToCart.blade.php
@@ -0,0 +1,37 @@
+<div>
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Product Details</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <form action="{{ route('cart.add') }}" method="post">
+                                        @csrf
+                                        <input type="hidden" name="prod_id" value="{{$product['prod_id']}}">
+                                        <label for="name">Product Name: </label>
+                                        <span>{{$product['prod_name']}}</span><br>
+                                        <label for="cname">Product Category: </label>
+                                        <span>{{$product['prod_category']}}</span><br>
+                                        <label for="price">Product Price: </label>
+                                        <span>PHP {{$product['prod_price']}}</span><br>
+                                        <label for="quantity">Quantity</label>
+                                        <input type="text" name="quantity" id="quantity" value="1" placeholder="Put a valid quantity"><br><br>
+                                        <button type="submit">Add to Cart</button><br>
+                                        <a href="{{ route('product.categories') }}" class="button btn">Back to Categories</a>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
