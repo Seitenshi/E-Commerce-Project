2ddff65e95378a07edf642734816cf47e6977aa1
Seitenshi <darkestdisguise@gmail.com>
2024-06-30 01:18:11 +0800

    a

---
+++ app/Models/ShoppingCart.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShoppingCart extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'order_id';
+    public $timestamps = false;
+    protected $fillable = [
+        'user_id',
+        'order_id',
+        'prod_id',
+        'prod_category',
+        'prod_name',
+        'prod_price',
+        'quantity',
+    ];
+}
