7c2f116531403422ddcd0e4b5247745a59cacaef
Seitenshi <darkestdisguise@gmail.com>
2024-06-30 01:17:21 +0800

    a

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'userLoggedIn' => \App\Http\Middleware\UserLoggedIn::class,
+        'isLoggedIn' => \App\Http\Middleware\CheckLoggedUser::class,
+        'noBack' => \App\Http\Middleware\PreventBackButton::class,
     ];
 }
