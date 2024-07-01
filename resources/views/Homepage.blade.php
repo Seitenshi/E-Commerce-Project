c4b02c55b19f1f3adbd14b4f4087e2b2ef11f74f
dcmp13 <cartz.cloud@gmail.com>
2024-07-01 22:46:20 +0800

    ngi

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -8,35 +8,34 @@
 
     <!-- Sticky Nav Bar -->
     <div class="topbar">
-        <nav class="navbar navbar-expand-lg fixed">
-            <div class="container-fluid">
-                <a class="navbar-brand ms-auto" href="#"><img src="/assets/img/klogo.png" height="50" width="50"></a>
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-header">
-                        <button type="button" class="btn-close"></button>
+            <nav class="navbar navbar-expand-lg fixed">
+                <div class="container-fluid">
+                    <a class="navbar-brand ms-auto" href="#"><img src="/assets/img/klogo.png" height="50" width="50"></a>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <button type="button" class="btn-close"></button>
+                        </div>
+                        <div class="offcanvas-body"> 
+                            <ul class="navbar-nav flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link mx-lg-2 active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-lg-2" href="{{ route('about') }}">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <button class="btn nav-link mx-lg-2"><i class="fa-solid fa-cart-shopping"></i></button>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="offcanvas-body"> 
-                        <ul class="navbar-nav flex-grow-1 pe-3">
-                            <li class="nav-item">
-                                <a class="nav-link mx-lg-2 active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link mx-lg-2" href="{{ route('about') }}">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                                <button class="btn nav-link mx-lg-2"><i class="fa-solid fa-cart-shopping"></i></button>
-                            </li>
-                        </ul>
-                    </div>
+                    <a href="{{ route('Login') }}" class="loginbutton">Login</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                 </div>
-                <a href="{{ route('Login') }}" class="loginbutton">Login</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
-        </nav>
-    </div>
-
+            </nav>
+        </div>
 
  <!-- Scroll break -->
 
@@ -48,21 +47,75 @@
 </div>
 </div>
 
-<div class="parlx1 w3-animate-left"> 
+{{-- <div class="parlx1 w3-animate-left"> 
     <div class="wrap">
         <div class="search">
                 <input type="text" class="searchTerm" name="search" placeholder="What are you looking for?">
                 <button type="submit" class="searchButton"><i class="fa fa-search"></i></button>
              </div>
           </div>
-</div>           
+</div>            --}}
 
 
+<div class="Midbar">
+    <video autoplay loop muted plays-inline class="vidback">
+        <source src="assets/vid/loopvid.mp4" type="video/mp4">
+    </video>
+    <h1>Welcome to Kalinaw Crafts</h1>
+    <p>Embrace the beauty of Filipino craftsmanship. Discover our unique collection of furniture and home essentials inspired by Filipino heritage.</p>
+    <div class="search-bar">
+        <input type="text" class="searchTerm" name="search" placeholder="What are you looking for?">
+        <button type="submit" class="searchButton"><i class="fa fa-search"></i></button>
+    </div>
+    <div class="hypel">
+        <a href="#FilipinoThemedChairs">Filipino Themed Chairs</a>
+        <a href="#NewArrivals">New Arrivals</a>
+        <a href="#SummaryFurnitureSale">Summary Furniture Sale</a>
+        <a href="#FilipinoIndoorComfort">Filipino Indoor Comfort</a>
+    </div>
+    <div>
+        <button type="button" id="shpbtn" class="btn btn-primary btn-lg">Shop Now</button>
+    </div>
+</div>
+
+<br><br><br>
+
+ <!-- Product highlights -->
+
+<div class="ProdShows">
+    <div class="ProdShow">
+        <div class="image-container">
+            <img src="/assets/img/sofa.jpg" alt="Handcrafted Sofa">
+        </div>
+        <div class="overlay">
+            <h3>Handcrafted Sofa</h3>
+            <a href="#">shop now</a>
+        </div>
+    </div>
+    <div class="ProdShow">
+        <div class="image-container">
+            <img src="/assets/img/rcks.jpg" alt="Filipino Racks">
+        </div>
+        <div class="overlay">
+            <h3>Filipino Racks</h3>
+            <a href="#">shop now</a>
+        </div>
+    </div>
+    <div class="ProdShow">
+        <div class="image-container">
+            <img src="/assets/img/cffetble.jpg" alt="Elegant Coffee Tables">
+        </div>
+        <div class="overlay">
+            <h3>Elegant Coffee Tables</h3>
+            <a href="#">shop now</a>
+        </div>
+    </div>
+</div>
     <br><br><br>
 
     <!-- Product highlights -->
 
-    <div class="carousel">
+    {{-- <div class="carousel">
   <div class="carousel__nav">
     <span id="moveLeft" class="carousel__arrow">
       <svg class="carousel__icon" width="24" height="24" viewBox="0 0 24 24">
@@ -91,31 +144,34 @@
       <a href="#" class="carousel-item__btn">Check more chairs</a>
     </div>
   </div>
-</div>
+</div> --}}
 
-
-
-
-<!-- 
-    <div class="container" style="padding: 2px">
-        <div class="row align-items-center justify-content-center" >
-            <div class="col-md-6">
-                Wood work pics
-            </div>
-
-            <div class="col-md-6">
-                <div class="row align-items-center justify-content-center" >
-                    Description
-                </div>
-                <br>
-                <div class="row align-items-center justify-content-center" >
-                    <button>View products</button>
-                </div>
-            </div>
+  <!--Section 3 to the last -->
+<div class="subscription-section">
+    <h2>Sign up for new stories and personal offers.</h2>
+    <p>Subscribe to get notified about new stories, news and personal offers.</p>
+    <form class="subscription-form">
+        <div class="input-group">
+            <span class="icon">@</span>
+            <input type="email" placeholder="Your email" required>
+            <button type="submit">→</button>
         </div>
-    </div> -->
+    </form>
+</div>
     <br><br><br>
 
+    <div class="inspiration-section">
+        <div class="inspiration-image">
+            <img src="/assets/img/chrinspi.jpg" alt="Inspiration Image">
+        </div>
+        <div class="inspiration-text">
+            <p class="inspiration-tag">INSPIRATION</p>
+            <h2>We are inspired by the realities of life today.</h2>
+            <a href="#" class="shop-button">Shop now</a>
+        </div>
+    </div>
+
+    <br><br><br>
 </div>
 <script src="/js/homepage.js"></script>
 </body>
