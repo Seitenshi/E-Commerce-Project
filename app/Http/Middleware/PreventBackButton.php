9063e073d91dee09f481092e54cc8ed7e9035cd8
Seitenshi <darkestdisguise@gmail.com>
2024-07-11 07:49:35 +0800

    middleware2

--- app/Http/Middleware/PreventBackButton.php
+++ app/Http/Middleware/PreventBackButton.php
@@ -17,14 +17,10 @@ class PreventBackButton
     public function handle($request, Closure $next)
     {
         $response = $next($request);
+        // Prevent caching sensitive pages after login
         $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
         $response->headers->set('Pragma', 'no-cache');
-        
-        $response->headers->set('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT');
-        
-        $response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT');
-        
-        $response->headers->set('Cache-Control', 'post-check=0, pre-check=0', false);
+        $response->headers->set('Expires', 'Sat, 01 Jan 1990 00:00:00 GMT');
 
         return $response;
     }
