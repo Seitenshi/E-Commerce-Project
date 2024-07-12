b8dc62d8c9ea86527d21b72cddcf6513be9fe4aa
Seitenshi <darkestdisguise@gmail.com>
2024-07-13 04:33:19 +0800

    products controller

--- app/Http/Controllers/Products.php
+++ app/Http/Controllers/Products.php
@@ -14,23 +14,26 @@ class Products extends Controller
     //Click CategoryA - leads to show all products under Category A. i.e. ProductChairList
     
     public function viewCategories(){
-
-        return view('Products.ProductCategory');
+        $totalcart = ShoppingCart::where('user_id',Session::get('LoggedUser'))->where('status','cart')->count(); //THE CART COUNT
+        return view('Products.ProductCategory','totalcart');
     }
 
     public function viewProductsPerCategory(Request $request){
+        $totalcart = ShoppingCart::where('user_id',Session::get('LoggedUser'))->where('status','cart')->count(); //THE CART COUNT
         $prod_category = $request->input('prod_category');
 
         $products = Product::where('prod_category', '=', $prod_category)->get();
+        $pic = Product::where('prod_category', '=', $prod_category)->pluck('file_name')->first();
 
         Tracker::where('note', $prod_category)->increment('visits');
 
-        return view('Products.ProductsPage', compact('prod_category','products'));
+        return view('Products.ProductsPage', compact('prod_category','products','pic','totalcart'));
     }
 
     public function viewProduct(Request $request){
+        $totalcart = ShoppingCart::where('user_id',Session::get('LoggedUser'))->where('status','cart')->count(); //THE CART COUNT
         $prod_id = $request->input('prod_id');
         $product = Product::where('prod_id', '=', $prod_id)->first();
-        return view('Products.viewProduct', compact('product'));
+        return view('Products.viewProduct', compact('product','totalcart'));
     }
 }
