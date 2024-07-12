d55a5df455a876e582ad329d9baf51bc18a389ed
Seitenshi <darkestdisguise@gmail.com>
2024-07-13 04:33:52 +0800

    locator

--- resources/views/storeLocator.blade.php
+++ resources/views/storeLocator.blade.php
@@ -1,3 +1,13 @@
 <div>
     <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+
+    <div class="mapouter">
+        <div class="gmap_canvas">
+            <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Lot%204,%20Block%208,%20E.%20Ragas%20Street,%20Pateros,%20Metro%20Manila&t=k&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <a href="https://embedgooglemap.net/124/">hot tub time machine 123movies</a><br>
+            <style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style>
+            <a href="https://www.embedgooglemap.net">embed google maps</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
+        </div>
+    </div>
+    
 </div>
