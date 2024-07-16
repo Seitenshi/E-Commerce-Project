9c34a971c3bb135b2f4f5cf62c9eab412cb867ed
Seitenshi <darkestdisguise@gmail.com>
2024-07-16 22:26:16 +0800

    products

--- resources/views/Products/ProductsPage.blade.php
+++ resources/views/Products/ProductsPage.blade.php
@@ -5,43 +5,46 @@
         <link rel="stylesheet" href="/css/productspage.css">
     </head>
 <body>
-<div>
-<!--STICKY NAVBAR -->
-<div class="topbar">
-    <nav class="navbar navbar-expand-lg fixed top">
-        <div class="container-fluid">
-            <a class="navbar-brand ms-auto" href="{{ route('home') }}"><img src="/assets/img/klogo.png" height="50" width="50"></a>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <button type="button" class="btn-close"></button>
+    <div>
+
+        <!-- Sticky Nav Bar -->
+        <div class="topbar">
+            <nav class="navbar navbar-expand-lg fixed top">
+                <div class="container-fluid">
+                    <a class="navbar-brand ms-auto" href="{{ route('home') }}"><img src="/assets/img/klogo.png" height="50" width="50"></a>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <button type="button" class="btn-close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link mx-lg-2" aria-current="page" href="{{ route('home') }}">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-lg-2" href="{{ route('logged.about') }}">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-lg-2" href="{{ route('product.categories') }}">Products</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-lg-2 active" href="{{ route('profile') }}"><img width="26" height="26" src="https://img.icons8.com/laces/64/gender-neutral-user.png" alt="gender-neutral-user"/></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ route('cart') }}" class="button btn nav-link mx-lg-2"><img width="26" height="26" src="https://img.icons8.com/ios/50/shopping-bag--v1.png" alt="shopping-bag--v1"/><sup class="cart-count">{{$totalcart}}</sup></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a href="{{ route('Logout') }}" class="loginbutton">Log out</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                 </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" aria-current="page" href="{{ route('home') }}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="{{ route('about') }}">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2 active" href="{{ route('product.categories') }}">Products</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="{{ route('profile') }}"><img width="26" height="26" src="https://img.icons8.com/laces/64/gender-neutral-user.png" alt="gender-neutral-user"/></a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('cart') }}" class="button btn nav-link mx-lg-2"><img width="26" height="26" src="https://img.icons8.com/ios/50/shopping-bag--v1.png" alt="shopping-bag--v1"/></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <a href="{{ route('Logout') }}" class="loginbutton">Log out</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            </nav>
         </div>
-    </nav>
-</div>
+    
+    </div>
 
 <!-- Others Products -->
 
